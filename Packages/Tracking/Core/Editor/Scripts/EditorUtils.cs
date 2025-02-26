317b23529bc06280d944a2ae3520faacccdb4182
tichise <43707+tichise@users.noreply.github.com>
2025-02-26 12:40:18 +0900

    Revert "Update EditorUtils.cs"
    This reverts commit 1d9244f339e975db7d946574ee4f1ff32030552f.

--- Packages/Tracking/Core/Editor/Scripts/EditorUtils.cs
+++ Packages/Tracking/Core/Editor/Scripts/EditorUtils.cs
@@ -84,8 +84,6 @@ namespace Leap
         /// <returns>toString of the serialized property's value</returns>
         public static string ValueToString(this SerializedProperty serializedProperty)
         {
-            if (serializedProperty == null) return "null";
-
             switch (serializedProperty.propertyType)
             {
                 case SerializedPropertyType.Integer:
@@ -95,15 +93,13 @@ namespace Leap
                 case SerializedPropertyType.Float:
                     return serializedProperty.floatValue.ToString();
                 case SerializedPropertyType.String:
-                    return serializedProperty.stringValue ?? "null";
+                    return serializedProperty.stringValue.ToString();
                 case SerializedPropertyType.Color:
                     return serializedProperty.colorValue.ToString();
                 case SerializedPropertyType.ObjectReference:
-                    return serializedProperty.objectReferenceValue?.ToString() ?? "null";
+                    return serializedProperty.objectReferenceValue.ToString();
                 case SerializedPropertyType.Enum:
-                    return serializedProperty.enumDisplayNames != null && serializedProperty.enumValueIndex >= 0 &&
-                           serializedProperty.enumValueIndex < serializedProperty.enumDisplayNames.Length ?
-                           serializedProperty.enumDisplayNames[serializedProperty.enumValueIndex] : "null";
+                    return serializedProperty.enumDisplayNames[serializedProperty.enumValueIndex];
                 case SerializedPropertyType.Vector2:
                     return serializedProperty.vector2Value.ToString();
                 case SerializedPropertyType.Vector3:
@@ -115,13 +111,13 @@ namespace Leap
                 case SerializedPropertyType.ArraySize:
                     return serializedProperty.arraySize.ToString();
                 case SerializedPropertyType.AnimationCurve:
-                    return serializedProperty.animationCurveValue?.ToString() ?? "null";
+                    return serializedProperty.animationCurveValue.ToString();
                 case SerializedPropertyType.Bounds:
                     return serializedProperty.boundsValue.ToString();
                 case SerializedPropertyType.Quaternion:
                     return serializedProperty.quaternionValue.ToString();
                 case SerializedPropertyType.ExposedReference:
-                    return serializedProperty.exposedReferenceValue?.ToString() ?? "null";
+                    return serializedProperty.exposedReferenceValue.ToString();
                 case SerializedPropertyType.FixedBufferSize:
                     return serializedProperty.fixedBufferSize.ToString();
                 case SerializedPropertyType.Vector2Int:
@@ -133,7 +129,7 @@ namespace Leap
                 case SerializedPropertyType.BoundsInt:
                     return serializedProperty.boundsIntValue.ToString();
                 case SerializedPropertyType.ManagedReference:
-                    return serializedProperty.managedReferenceValue?.ToString() ?? "null";
+                    return serializedProperty.managedReferenceValue.ToString();
                 case SerializedPropertyType.Hash128:
                     return serializedProperty.hash128Value.ToString();
                 case SerializedPropertyType.Generic:
